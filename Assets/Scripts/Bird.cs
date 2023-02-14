dadf9fb6a153fd1527a4d34ae137ac33049118e0
Michael Rivera <mgrivera997@gmail.com>
2023-02-14 22:10:21 +0800

    Added basic random movement for Bird prefab
    as the summary says.

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -1,16 +1,21 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
-    public float Speed;
+    public float speed;
+    public Rigidbody2D rb;
 
+    public double speedIncrement;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Move();
+        StartCoroutine("ChangeDirection");
+
+        speedIncrement = 0.1;
     }
 
     // Update is called once per frame
@@ -22,5 +27,26 @@ public class Bird : MonoBehaviour
     public void OnMouseDown()
     {
         Debug.Log("Down");
+    }
+
+    public void Move()
+    {
+        float x = Random.Range(0,2) == 0 ? -1 : 1;
+        float y = Random.Range(0,2) == 0 ? -1 : 1;
+
+        Debug.Log(x);
+        Debug.Log(y);
+        rb.velocity = new Vector2(speed * x, speed * y);
+    }
+
+    IEnumerator ChangeDirection() 
+    {
+        WaitForSeconds waitTime = new WaitForSeconds(2);
+        while (true) 
+        {
+            Move();
+            speed += (float)speedIncrement;
+            yield return waitTime;
+        }
     }
 }
