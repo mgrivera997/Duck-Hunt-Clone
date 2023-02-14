32da94db5fc1dfdd76deba443aaa5adb986346cc
Paul Andrew Lopez <paullopez0230@gmail.com>
2023-02-14 23:51:00 +0800

    Added game manager + fixed crosshair

--- Assets/Scripts/Gun/Crosshair.cs
+++ Assets/Scripts/Gun/Crosshair.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Crosshair : MonoBehaviour
 {
+    [SerializeField] private Camera mainCamera;
     void Update()
     {
-        Vector3 mousePos = Input.mousePosition;
-        this.transform.localPosition = mousePos;
+        Vector3 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f;
+        transform.position = mousePos;
+        
     }
 }
---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int bulletMaxCount;
+    public int bulletCurCount;
+
+    public int duckMaxCount;
+    public int duckCurCount;
+
+    public int score;
+
+    
+}
