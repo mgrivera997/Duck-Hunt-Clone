48d21bd3ef541fe0fe7864edfff499981216d6f4
Michael Rivera <mgrivera997@gmail.com>
2023-02-21 21:21:08 +0800

    Added scoreValue variable to Bird script

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -6,8 +6,11 @@ public class Bird : MonoBehaviour
 {
     public float speed;
     public Rigidbody2D rb;
+    public int scoreValue;
 
     public double speedIncrement;
+
+    
 
     // Start is called before the first frame update
     void Start()
